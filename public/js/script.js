8475cc33ecac561ec04ab4d93a51abd93012b7a1
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2023-08-29 22:39:31 +0300

    cleanup js code

--- public/js/script.js
+++ public/js/script.js
@@ -1,103 +1,81 @@
-// Load the JSON file
-fetch('.\\public\\json\\projects.json')
-.then(response => response.json())
-.then(data => {
-    let html = '';
-    let html_projects_page = '';
-    let count = 0;
+const projectsContainer = document.getElementById('card-slider');
+const projectsAllContainer = document.getElementById('card-slider-all');
+const projectsCountElement = document.getElementById('projects-count');
+const yearElement = document.getElementById('year');
 
-    // loop for homepage
-    data.forEach((object, index) => {
-        if(index > 3) return;
+const fetchProjects = async () => {
+    try {
+        const response = await fetch('./public/json/projects.json');
+        const data = await response.json();
 
-        html += `
-            <a href="${object.link}" target="_blank" id="card-${index+1}" onmouseover="onHoverCard(${index+1}, '${object?.image_animated || ''}')" onmouseout="onHoverCard(${index+1}, '${object.image}')"`
-            
-            if(object.link == ""){
-                // console.log("HERE")
-                html += `class="card inactive-link"`;
-            }else{
-                html += `class="card"`;
-            }
+        let html = '';
+        let htmlProjectsPage = '';
+        let count = 0;
 
-            html +=
-            `>
-                <div class="card-image">
-                    <img onmouseover="this.src = '${object?.image_animated || ''}'" onmouseout="this.src = '${object.image}'" alt="Project ${index+1}" id="card-img-${index+1}" src="${object.image}"> 
+        const createCardHTML = (object, index, containerId) => {
+            const imageSrc = object?.image_animated || object.image;
+            const cardClass = object.link ? 'card' : 'card inactive-link';
 
-                </div>
-                <div class="card-content">
-                    <h3 class="card-title fs-118">${object.title} <span class="font-color-secondary fs-6 fst-italic">${object?.status ? `(${object?.status})` : ""}</span></h3>
-                    <p class="card-description">${object.description}</p>`
-                    
-                    if(object.date != ""){
-                        html += 
-                        `
-                        <div class="date-content">
-                            <p class="card-date">${object.date}</p>
-                        </div>
-                        `
-                    }
-
-            html +=
-                        `
+            return `
+                <a href="${object.link}" target="_blank"
+                    id="card-${index + 1}"
+                    onmouseover="onHoverCard(${index + 1}, '${object?.image_animated || ''}')"
+                    onmouseout="onHoverCard(${index + 1}, '${object.image}')"
+                    class="${cardClass}">
+                    <div class="card-image">
+                        <img onmouseover="this.src = '${object?.image_animated || ''}'"
+                            onmouseout="this.src = '${object.image}'"
+                            alt="Project ${index + 1}"
+                            id="card-img-${index + 1}"
+                            src="${imageSrc}">
                     </div>
-                </a>
-        `
-    });
+                    <div class="card-content">
+                        <h3 class="card-title fs-118">${object.title} 
+                            <span class="font-color-secondary fs-6 fst-italic">
+                                ${object?.status ? `(${object.status})` : ''}
+                            </span>
+                        </h3>
+                        <p class="card-description">${object.description}</p>
+                        ${object.date ? `<div class="date-content"><p class="card-date">${object.date}</p></div>` : ''}
+                    </div>
+                </a>`;
+        };
 
-    // loop for projects page
-    data.forEach((object, index) => {
-        html_projects_page += `
-            <a href="${object.link}" target="_blank" id="card-${index+1}" onmouseover="onHoverCard(${index+1}, '${object?.image_animated || ''}')" onmouseout="onHoverCard(${index+1}, '${object.image}')"`
+        data.slice(0, 4).forEach((object, index) => {
+            html += createCardHTML(object, index, 'card-slider');
+        });
 
-            if(object.link == ""){
-                html_projects_page += `class="card inactive-link"`;
-            }else{
-                html_projects_page += `class="card"`;
-            }
+        data.forEach((object, index) => {
+            htmlProjectsPage += createCardHTML(object, index, 'card-slider-all');
+            count++;
+        });
 
-            html_projects_page +=
-            ` ">
-                <div class="card-image">
-                    <img onmouseover="this.src = '${object?.image_animated || ''}'" onmouseout="this.src = '${object.image}'" alt="Project ${index+1}" id="card-img-${index+1}" src="${object.image}"> 
+        if (projectsContainer) {
+            projectsContainer.innerHTML += html;
+        }
 
-                </div>
-                <div class="card-content">
-                    <h3 class="card-title fs-118">${object.title} <span class="font-color-secondary fs-6 fst-italic">${object?.status ? `(${object?.status})` : ""}</span></h3>
-                    <p class="card-description">${object.description}</p>`
+        if (projectsAllContainer) {
+            projectsAllContainer.innerHTML += htmlProjectsPage;
+        }
 
-                    if(object.date != ""){
-                        html_projects_page += 
-                        `
-                        <div class="date-content">
-                            <p class="card-date">${object.date}</p>
-                        </div>
-                        `
-                    }
+        if (projectsCountElement) {
+            projectsCountElement.innerHTML = `(${count})`;
+        }
 
-                    html_projects_page +=
-                    `
-                </div>
-            </a>
-        `
-        count++;
-    
-    })
+        if (yearElement) {
+            yearElement.innerHTML = new Date().getFullYear();
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
 
-    let projects = document.getElementById('card-slider');
-    let projects_all = document.getElementById('card-slider-all');
-
-    if(projects) projects.innerHTML += html;
-    if(projects_all) projects_all.innerHTML += html_projects_page;
-
-    document.getElementById('projects-count').innerHTML = `(${count})`
-    document.getElementById("year").innerHTML = new Date().getFullYear();
-})
-.catch(error => console.error(error));
-
-
-// onhover change image on all card element
-function onHoverCard(index, src){
-    document.getElementById(`card-img-${index}`).src = src
+// onhover change image on all card elements
+function onHoverCard(index, src) {
+    const cardImage = document.getElementById(`card-img-${index}`);
+    if (cardImage) {
+        cardImage.src = src;
+    }
 }
+
+fetchProjects();
